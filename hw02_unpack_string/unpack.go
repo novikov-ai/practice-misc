5e95636a1b94a54662a1540d872e5e18de37c4b3
Aleksandr <al.novikov08@gmail.com>
2022-05-15 13:19:57 +0300

    implement Unpack() func

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,53 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+
+	inputRunes := []rune(input)
+	inputRunesCount := len(inputRunes)
+
+	if input == "" {
+		return "", nil
+	}
+
+	if unicode.IsDigit(inputRunes[0]) {
+		return "", ErrInvalidString
+	}
+
+	var unpackedInput strings.Builder
+
+	for i := 0; i < inputRunesCount; i++ {
+
+		currentRune := inputRunes[i]
+
+		isCurrentRuneDigit := unicode.IsDigit(currentRune)
+		if isCurrentRuneDigit {
+			return "", ErrInvalidString
+		}
+
+		repeatNumber := 1
+
+		if i+1 < inputRunesCount {
+
+			nextRune := inputRunes[i+1]
+
+			skipNextRune := unicode.IsDigit(nextRune)
+			if skipNextRune {
+				repeatNumber, _ = strconv.Atoi(string(nextRune))
+				i++
+			}
+		}
+
+		repeatedValue := strings.Repeat(string(currentRune), repeatNumber)
+		unpackedInput.WriteString(repeatedValue)
+	}
+
+	return unpackedInput.String(), nil
 }
