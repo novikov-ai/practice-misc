c24e1282da3b42c4a4ec66957e40c796340c4793
Aleksandr <al.novikov08@gmail.com>
2022-05-22 21:10:44 +0300

    completed main task

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type UniqueWord struct {
+	Name   string
+	Amount int
+}
+
+func Top10(s string) []string {
+
+	const topBoundNumber = 10
+
+	inputSplit := strings.Fields(s)
+
+	words := make(map[string]int)
+
+	for _, v := range inputSplit {
+
+		elem, ok := words[v]
+		if ok {
+			words[v] = elem + 1
+		} else {
+			words[v] = 1
+		}
+	}
+
+	if len(words) == 0 {
+		return nil
+	}
+
+	sortedWords := make([]UniqueWord, 0)
+	for key, value := range words {
+		sortedWords = append(sortedWords, UniqueWord{
+			Name:   key,
+			Amount: value})
+	}
+
+	sort.Slice(sortedWords, func(i, j int) bool {
+
+		isAmountEqual := sortedWords[i].Amount == sortedWords[j].Amount
+		if isAmountEqual {
+			return sortedWords[i].Name < sortedWords[j].Name
+		}
+
+		return sortedWords[i].Amount > sortedWords[j].Amount
+	})
+
+	result := make([]string, topBoundNumber)
+
+	i := 0
+	for _, value := range sortedWords {
+		if i < topBoundNumber {
+			result[i] = value.Name
+			i++
+		}
+	}
+
+	return result
 }
