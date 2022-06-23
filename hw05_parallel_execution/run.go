827e07889e00b3d035895b71dd93dc4e6045ff96
Aleksandr <al.novikov08@gmail.com>
2022-06-23 12:41:28 +0300

    refactor

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -5,13 +5,29 @@ import (
 	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-var ErrErrorsInvalidArgs = errors.New("invalid arguments")
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	ErrErrorsInvalidArgs   = errors.New("invalid arguments")
+)
 
 type Task func() error
 
 type ErrorsLimitCalc struct {
 	Current, Max int
+	Mutex        sync.RWMutex
+}
+
+func (errorsCalc *ErrorsLimitCalc) LimitExceeded() bool {
+	errorsCalc.Mutex.RLock()
+	defer errorsCalc.Mutex.RUnlock()
+	return errorsCalc.Current >= errorsCalc.Max
+}
+
+func (errorsCalc *ErrorsLimitCalc) IncrementErrors() {
+	errorsCalc.Mutex.Lock()
+	defer errorsCalc.Mutex.Unlock()
+
+	errorsCalc.Current++
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -25,13 +41,11 @@ func Run(tasks []Task, n, m int) error {
 	}
 
 	jobs := make(chan Task)
-	errorsCalc := ErrorsLimitCalc{Current: 0, Max: m}
-
-	mx := sync.RWMutex{}
-	wg := sync.WaitGroup{}
+	errorsCalc := &ErrorsLimitCalc{Current: 0, Max: m, Mutex: sync.RWMutex{}}
+	wg := &sync.WaitGroup{}
 
 	for i := 0; i < n; i++ {
-		go consumer(jobs, &errorsCalc, &mx, &wg)
+		go consumer(jobs, errorsCalc, wg)
 	}
 
 	defer wg.Wait()
@@ -39,7 +53,7 @@ func Run(tasks []Task, n, m int) error {
 
 	for _, task := range tasks {
 		jobs <- task
-		if errorsCalc.LimitExceeded(&mx) {
+		if errorsCalc.LimitExceeded() {
 			return ErrErrorsLimitExceeded
 		}
 	}
@@ -47,22 +61,16 @@ func Run(tasks []Task, n, m int) error {
 	return nil
 }
 
-func (errorsCalc *ErrorsLimitCalc) LimitExceeded(mx *sync.RWMutex) bool {
-	mx.RLock()
-	defer mx.RUnlock()
-	return errorsCalc.Current >= errorsCalc.Max
-}
-
-func consumer(jobs <-chan Task, errorsCalc *ErrorsLimitCalc, mx *sync.RWMutex, wg *sync.WaitGroup) {
+func consumer(jobs <-chan Task, errorsCalc *ErrorsLimitCalc, wg *sync.WaitGroup) {
 	defer wg.Done()
 	wg.Add(1)
 
 	for job := range jobs {
 		err := job()
-		if err != nil {
-			mx.Lock()
-			errorsCalc.Current++
-			mx.Unlock()
+		if err == nil {
+			continue
 		}
+
+		errorsCalc.IncrementErrors()
 	}
 }
