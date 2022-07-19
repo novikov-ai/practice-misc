67bc2e7a02d1bcd065ff40f291a69b7d9b03ee38
Aleksandr <al.novikov08@gmail.com>
2022-07-19 22:26:46 +0300

    small fix

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,49 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env.
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	err := configEnvironment(env)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command := exec.Cmd{
+		Path:   cmd[0],
+		Env:    os.Environ(),
+		Args:   cmd,
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	err = command.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return 0
+}
+
+func configEnvironment(env Environment) error {
+	for envName, envValue := range env {
+		err := os.Setenv(envName, envValue.Value)
+		if err != nil {
+			return err
+		}
+
+		if !envValue.NeedRemove {
+			continue
+		}
+
+		err = os.Unsetenv(envName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
