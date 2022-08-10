e407469fc85c763dc396d059ca962ff3026fd033
Aleksandr <al.novikov08@gmail.com>
2022-08-10 18:58:30 +0300

    init telnet client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,101 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+)
+
+const (
+	FlagTimeout = "timeout"
+)
+
+var (
+	Timeout    time.Duration
+	ErrTimeout = errors.New("timeout reached")
+)
+
+func init() {
+	flag.DurationVar(&Timeout, FlagTimeout, 10*time.Second, "timeout for connection")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	hostPort := getHostAddress()
+
+	tc := NewTelnetClient(hostPort, Timeout, os.Stdin, os.Stdout)
+	defer tc.Close()
+
+	err := tc.Connect()
+	if err != nil {
+		log.Fatal(ErrTimeout)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		telnetClientDo(ctx, tc.Send)
+		stop()
+	}()
+	go func() {
+		telnetClientDo(ctx, tc.Receive)
+		stop()
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println()
+		stop()
+	}
+}
+
+func logError(r error) {
+	_, err := fmt.Fprintf(os.Stderr, "%s\n", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getHostAddress() string {
+	arguments := os.Args
+	for i, arg := range arguments {
+		if strings.Contains(arg, FlagTimeout) {
+			left := arguments[0:i]
+			right := arguments[i+1:]
+			arguments = append(left, right...)
+			break
+		}
+	}
+
+	if len(arguments) < 3 {
+		log.Fatal("Please provide host and port")
+	}
+
+	return fmt.Sprintf("%s:%s", arguments[1], arguments[2])
+}
+
+func telnetClientDo(ctx context.Context, work func() error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			err := work()
+			if errors.Is(io.EOF, err) {
+				logError(err)
+				return
+			} else if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,53 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Client struct {
+	address    string
+	timeout    time.Duration
+	input      io.ReadCloser
+	output     io.Writer
+	connection net.Conn
+}
+
+func (cl *Client) Connect() error {
+	conn, err := net.DialTimeout("tcp", cl.address, cl.timeout)
+	if err != nil {
+		return err
+	}
+
+	cl.connection = conn
+
+	fmt.Printf("Welcome to %s!\n", cl.address)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (cl *Client) Close() error {
+	return cl.connection.Close()
+}
+
+func (cl *Client) Send() error {
+	r := bufio.NewReader(cl.input)
+	message, err := r.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	_, err = cl.connection.Write([]byte(message))
+	return err
+}
+
+func (cl *Client) Receive() error {
+	r := bufio.NewReader(cl.connection)
+
+	message, err := r.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(cl.output, message)
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Client{address: address, timeout: timeout, input: in, output: out}
+}
