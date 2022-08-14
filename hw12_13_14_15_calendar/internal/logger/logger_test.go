6b295873c8719a3a9f98daa15f83df60315b459b
Aleksandr <al.novikov08@gmail.com>
2022-08-14 23:15:47 +0300

    add logger & tests

--- hw12_13_14_15_calendar/internal/logger/logger_test.go
+++ hw12_13_14_15_calendar/internal/logger/logger_test.go
@@ -1,7 +1,75 @@
 package logger
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+)
 
 func TestLogger(t *testing.T) {
-	// TODO
+	testCases := []struct {
+		logLevel string
+		output   []string
+	}{
+		{logLevel: "DEBUG", output: []string{"DEBUG", "INFO", "WARN", "ERROR"}},
+		{logLevel: "INFO", output: []string{"INFO", "WARN", "ERROR"}},
+		{logLevel: "WARN", output: []string{"WARN", "ERROR"}},
+		{logLevel: "ERROR", output: []string{"ERROR"}},
+	}
+
+	for _, test := range testCases {
+		test := test
+
+		t.Run(fmt.Sprintf("Testing log-level: %s\n", test.logLevel), func(t *testing.T) {
+			logger := New(Config{Level: test.logLevel})
+
+			w := CustomWriter{}
+			logger.Writer = &w
+
+			logged := make([]string, 0)
+
+			logger.Debug("...")
+			flushed := w.Flush()
+			if flushed != "" {
+				logged = append(logged, flushed)
+			}
+
+			logger.Info("...")
+			flushed = w.Flush()
+			if flushed != "" {
+				logged = append(logged, flushed)
+			}
+
+			logger.Warn("...")
+			flushed = w.Flush()
+			if flushed != "" {
+				logged = append(logged, flushed)
+			}
+
+			logger.Error("...")
+			flushed = w.Flush()
+			if flushed != "" {
+				logged = append(logged, flushed)
+			}
+
+			require.Equal(t, len(test.output), len(logged))
+
+			for i := 0; i < len(logged); i++ {
+				require.True(t, strings.Contains(logged[i], test.output[i]))
+			}
+		})
+	}
+}
+
+type CustomWriter struct {
+	content strings.Builder
+}
+
+func (w *CustomWriter) Write(p []byte) (n int, err error) {
+	return w.content.Write(p)
+}
+func (w *CustomWriter) Flush() string {
+	defer w.content.Reset()
+	return w.content.String()
 }
