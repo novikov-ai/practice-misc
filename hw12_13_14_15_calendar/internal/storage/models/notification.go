62ee3579051fde0a29b5c786d0e96dcf762d3dc1
Aleksandr <al.novikov08@gmail.com>
2022-08-23 22:54:57 +0300

    rename

--- hw12_13_14_15_calendar/internal/storage/models/notification.go
+++ hw12_13_14_15_calendar/internal/storage/models/notification.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Notification struct {
-	Date  time.Time
-	ID    string
-	Title string
-	User  string
+	Date   time.Time
+	ID     string
+	Title  string
+	UserID string
 }
