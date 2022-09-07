7de8c5a213b4982e67682cdc4f8f3df51b10bebf
Aleksandr <al.novikov08@gmail.com>
2022-09-07 18:46:51 +0300

    add sender microservice

--- hw12_13_14_15_calendar/cmd/sender/main.go
+++ hw12_13_14_15_calendar/cmd/sender/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/novikov-ai/practice-misc/hw12_13_14_15_calendar/configs"
+	"github.com/novikov-ai/practice-misc/hw12_13_14_15_calendar/internal/sender"
+	"github.com/novikov-ai/practice-misc/hw12_13_14_15_calendar/pkg/ampq"
+	log "github.com/novikov-ai/practice-misc/hw12_13_14_15_calendar/pkg/logger"
+	"github.com/novikov-ai/practice-misc/hw12_13_14_15_calendar/pkg/utils"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "configs/config_scheduler.toml", "Path to configuration file")
+}
 
 func main() {
-	fmt.Println("SENDER")
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		utils.PrintVersion()
+		return
+	}
+
+	config := configs.NewConfigSender(configFile)
+
+	logger := log.New(&config)
+
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	messageBroker := ampq.New(&config, logger)
+	err := messageBroker.Connect()
+	if err != nil {
+		exitWithError(logger, err)
+	}
+	defer messageBroker.Close()
+
+	sender := sender.New(logger, config, messageBroker)
+
+	errs := make(chan error, 1)
+
+	go func() {
+		errs <- sender.Start(ctx, os.Stdout)
+	}()
+
+	select {
+	case <-ctx.Done():
+		logger.Info("sender stopped worked by cancellation")
+		break
+	case err := <-errs:
+		exitWithError(logger, err)
+	}
+}
+
+func exitWithError(log *log.Logger, err error) {
+	log.Error(err.Error())
+	os.Exit(1)
 }
